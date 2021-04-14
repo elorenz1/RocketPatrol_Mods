f7ac6cf6ee5b64612997833fa16885cac860c03e
elorenz1 <elorenz@ucsc.edu>
2021-04-13 19:32:09 -0700

    added 2 sounds

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -6,7 +6,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
         this.isFiring = false; // track rocket firing status
         this.moveSpeed = 2; // pixels per frame
-
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx boi
     }
 
     update() {
@@ -20,8 +20,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
 
         // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();
         }
 
         // if fired, move the rocket up
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -78,7 +78,7 @@ class Play extends Phaser.Scene{
 
         // 60-second play clock    
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(60000, () => {
+        this.clock = this.time.delayedCall(5000, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart',
                 scoreConfig).setOrigin(0.5);
@@ -87,6 +87,11 @@ class Play extends Phaser.Scene{
     }
 
     update(){
+        // check key input for restart
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }
+
         this.starfield.tilePositionX -= starSpeed;
 
         if (!this.gameOver) {
@@ -137,6 +142,8 @@ class Play extends Phaser.Scene{
         // score add and repaint
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
+
+        this.sound.play('sfx_explosion');
     }
     
 }
