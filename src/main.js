94523e764266bffeb092ca396ccc527010ab15f2
elorenz1 <elorenz@ucsc.edu>
2021-04-21 15:07:45 -0700

    added points breakdown comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
+/* *********************************************
+Name: Emersen Lorenz
+Project Title: RocketPatrol_Mods
+Time to Complete: 11 hours
+Points BreakDown:
+    - high score tracker (5 pts)
+    - FIRE UI text (5 pts)
+    - my own background music (5 pts)
+    - speed increase (5 pts)
+    - randomized enemy ship direction (5 pts)
+    - new scrolling background sprite (5 pts)
+    - player control after firing rocket (5 pts)
+    - 4 new explosion fx (10 pts)
+    - time remaining (10 pts)
+    - new enemy spaceship (20 pts)
+    - mouse control (20 pts)
+    - final point count: 95
+    - thoughts: USE mouse instead of arrow keys
+    because the mouse will override it usually.
+************************************************/
+
 // game configuration lol
 let config = {
     type: Phaser.CANVAS,
