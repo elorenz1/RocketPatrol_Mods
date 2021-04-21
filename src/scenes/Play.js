93807496e3c4476d1cdadf9df1c5409d2e53f72c
elorenz1 <elorenz@ucsc.edu>
2021-04-20 21:15:42 -0700

    new background image

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,7 +8,7 @@ class Play extends Phaser.Scene{
         // load images/tile sprites
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
-        this.load.image('starfield', './assets/starfield.png');
+        this.load.image('starfield', './assets/spirals.png');
         // load spritesheet
         this.load.spritesheet('explosion', './assets/explosion.png', {
             frameWidth: 64,
